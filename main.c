4c01a8e4f5ff82280c64e60d7ddef897770cf18c
Dismal <44642560+trehousecomicsDev@users.noreply.github.com>
2023-01-23 15:40:20 +0000

    Completed the 2nd version

--- main.c
+++ main.c
@@ -1,27 +1,15 @@
 #include <stdio.h>
-
 #include <string.h>
 
-void   reverse(char* string){
+int main() {
 
-    static int i, l, temptemporary_var;
-   l = strlen(string);
-    if (i < l / 2) {
-        temptemporary_var = string[i];
-        string[i] = string[l - i - 1];
-        string[l - i - 1] = temptemporary_var;
-        i++;
-        reverse(string);
-    }
-}
-int main()
-{
-    char  string[40];
-    printf(" Enter the string: ");
-    gets(string);
-    printf(" \n Before reversing the string: %s \n", string);
-    revstr(string);
-    printf(" After reversing the string: %s", string);
+  char s1[] = "Hello, World!";
+  int len = strlen(s1) -1;
 
-
+  while(len >=0) {
+    printf("%c\n", s1[len]);
+    len--;
+  }
+  
+  return 0;
 }
